1287f5e2f04776544be2fe6d6f3a78a88a56d982
bobi54y <uuu400424@gmail.com>
2025-07-01 13:55:55 +0300

    роблю меню  3

--- Assets/Menu/menuScript/mainMenu.cs
+++ Assets/Menu/menuScript/mainMenu.cs
@@ -1,18 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class mainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+   public void PlayGame()
     {
-        
-    }
+        SceneManager.LoadScene("Game1");
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
 
-    // Update is called once per frame
-    void Update()
+    }
+    public void ExitGame()
     {
-        
+        Debug.Log("Exit");
+        Application.Quit();
     }
 }
