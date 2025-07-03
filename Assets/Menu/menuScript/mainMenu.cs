22411ea4455c69bb3ffb9ed594625d45e7ed2143
bobi54y <uuu400424@gmail.com>
2025-07-03 16:56:58 +0300

    сделал настройки графики

--- Assets/Menu/menuScript/mainMenu.cs
+++ Assets/Menu/menuScript/mainMenu.cs
@@ -7,8 +7,8 @@ public class mainMenu : MonoBehaviour
 {
    public void PlayGame()
     {
-        SceneManager.LoadScene("Game1");
-        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        //SceneManager.LoadScene("Game1");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
 
     }
     public void ExitGame()
