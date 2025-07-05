d6a5b9c5bd913509b2c3b96a3625faabbb3161d6
asfsghg <artemryzenka@gmail.com>
2025-07-05 19:52:39 +0300

    turret

--- Assets/Core/Bullet.cs
+++ Assets/Core/Bullet.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] public int _damage = 10;
+    private GameObject target;
+    private float _destroyTime = 1f;
+
+    private void OnTriggerEnter(Collider other)
     {
-        
+
+        if (other.TryGetComponent<Monster>(out var player))
+        {
+            player._health -= _damage;
+            Destroy(gameObject);
+            Debug.Log(player._health);
+            if (player._health <= 0)
+            {
+                Destroy(player.gameObject);
+            }
+
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (_destroyTime <= 0)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            _destroyTime -= Time.deltaTime;
+            Debug.Log(_destroyTime);
+        }
     }
 }
+
--- Assets/Core/Camera Control.cs
+++ Assets/Core/Camera Control.cs
@@ -1,21 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-
 public class CameraController : MonoBehaviour
 {
     private Camera cam;
-    
-
     void Start()
     {
         cam = Camera.main;
     }
-
     void Update()
     {
         float horizontal = Input.GetAxis("Horizontal");
-        
+        float vertical = Input.GetAxis("Vertical");
         cam.transform.Translate(Vector3.right * horizontal);
+        cam.transform.Translate(Vector3.forward * vertical);
     }
 }
