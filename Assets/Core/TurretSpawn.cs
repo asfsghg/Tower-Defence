d299c6dd654d7de1507d76269ee5880598883209
asfsghg <artemryzenka@gmail.com>
2025-07-10 18:45:49 +0300

    bug fix yeeaa

--- Assets/Core/TurretSpawn.cs
+++ Assets/Core/TurretSpawn.cs
@@ -1,32 +1,36 @@
 using System;
 using UnityEngine;
 using System.Collections.Generic;
+using UnityEngine.Video;
 
 public class BuildingPlacer : MonoBehaviour
 {
-   public GameObject buildingPrefab; //префаб турелі
-   private GameObject buildingInstance; // позиція турелі через рейкаст
-   public Vector3 mouseWorldPosition; // миша
+    public GameObject buildingPrefab; // префаб турелі
+    private GameObject buildingInstance; // посилання на створений префаб
+    public Vector3 mouseWorldPosition; // позиція миші
+    private float time = 2f; //затримка між спавном
 
-   void Update()
-   {
-      if (Input.GetMouseButtonDown(0))
-      {
-         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-   
-         if (Physics.Raycast(ray, out RaycastHit hit))
-         {
-            if (hit.collider.tag == "Building")
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Build();
+        }
+    }
+
+    private void Build()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Debug.DrawRay(ray.origin, ray.direction * 10, Color.red);
+
+        if (Physics.Raycast(ray, out RaycastHit hit))
+        {
+            if (hit.collider.CompareTag("Building"))
             {
-               mouseWorldPosition = hit.point;
-               buildingInstance = Instantiate(buildingPrefab.gameObject, mouseWorldPosition, Quaternion.identity);
-               buildingInstance.transform.parent = transform;
-               hit.collider.tag = "Untagged";
-               
+                mouseWorldPosition = hit.point;
+                buildingInstance = Instantiate(buildingPrefab, mouseWorldPosition, Quaternion.identity);
+                hit.collider.tag = "Untagged";
             }
-         }
-      }
-      
-   }
-   
+        }
+    }
 }
