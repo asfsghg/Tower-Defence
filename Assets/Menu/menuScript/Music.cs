71fe0597a8ca016df3cabf63ee721a20be10fd8e
bobi54y <uuu400424@gmail.com>
2025-07-19 12:38:01 +0300

    -баг +фича

--- Assets/Menu/menuScript/Music.cs
+++ Assets/Menu/menuScript/Music.cs
@@ -6,25 +6,19 @@ public class AudioController : MonoBehaviour
 {
     public  AudioSource audio;
     public AudioClip clip;
+
     public Sprite audioOn;
     public Sprite audioOff;
     public Sprite buttonAudio;
+
     public Image buttonImage; 
-
-
 
     public Slider slider;
 
-
-    void Start()
-    {
-        
-    }
     void Update()
     {
         audio.volume = slider.value; 
     }
-
     public void OnffAudio()
     {
         if (AudioListener.volume == 1)
@@ -38,12 +32,10 @@ public class AudioController : MonoBehaviour
             buttonAudio.GetComponent<Image>().sprite = audioOn;
         }
     }
-
     public void PlaySound()
     {
         audio.PlayOneShot(clip);
     }
-
 }
 
 
