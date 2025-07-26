9d13919369fb627593ddd70b605136842b2e046e
bobi54y <uuu400424@gmail.com>
2025-07-26 13:10:44 +0300

    Эффекты звук выстрела башни
    найтройте как вам нужно и вставте в скрипт стрельбы owerShoot.Shoot();

---
+++ Assets/sound effects/script/shotSpund.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class owerShoot : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public AudioClip shootSound;  // звук выстрела
+    public AudioClip hitSound;    // звук попадания 
+
+    void Start()
+    {
+        if (audioSource == null)
+            audioSource = GetComponent<AudioSource>();
+    }
+
+    public void Shoot()
+    {
+        audioSource.PlayOneShot(shootSound);
+    }
+
+    public void HitTarget()
+    {
+        if (hitSound != null)
+            audioSource.PlayOneShot(hitSound);
+    }
+}
+
+
