1a664c76b3a937de2085d07787cc5936414fe55e
bobi54y <uuu400424@gmail.com>
2025-07-31 13:47:48 +0300

    движения камеры готово

---
+++ Assets/file level 2/script/CameraMovement.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public float moveSpeed = 20f;
+    public float edgeScrollSize = 10f; 
+    public float zoomSpeed = 10f;
+    public float minY = 10f;
+    public float maxY = 100f;
+
+    void Update()
+    {
+        Vector3 pos = transform.position;
+
+       
+        if (Input.GetKey("s") || Input.GetKey(KeyCode.UpArrow))
+            pos.z += moveSpeed * Time.deltaTime;
+        if (Input.GetKey("w") || Input.GetKey(KeyCode.DownArrow))
+            pos.z -= moveSpeed * Time.deltaTime;
+        if (Input.GetKey("a") || Input.GetKey(KeyCode.RightArrow))
+            pos.x += moveSpeed * Time.deltaTime;
+        if (Input.GetKey("d") || Input.GetKey(KeyCode.LeftArrow))
+            pos.x -= moveSpeed * Time.deltaTime;
+
+       
+        if (Input.mousePosition.y >= Screen.height - edgeScrollSize)
+            pos.z += moveSpeed * Time.deltaTime;
+        if (Input.mousePosition.y <= edgeScrollSize)
+            pos.z -= moveSpeed * Time.deltaTime;
+        if (Input.mousePosition.x >= Screen.width - edgeScrollSize)
+            pos.x += moveSpeed * Time.deltaTime;
+        if (Input.mousePosition.x <= edgeScrollSize)
+            pos.x -= moveSpeed * Time.deltaTime;
+
+        
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        pos.y -= scroll * zoomSpeed * 100f * Time.deltaTime;
+        pos.y = Mathf.Clamp(pos.y, minY, maxY);
+
+        transform.position = pos;
+    }
+}
