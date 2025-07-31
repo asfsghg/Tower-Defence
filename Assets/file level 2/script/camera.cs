ea699989b6567cbab04e3c8e0e5c31fb32ca2443
bobi54y <uuu400424@gmail.com>
2025-07-31 12:49:57 +0300

    --- баг но нечего не получается

--- Assets/file level 2/script/camera.cs
+++ Assets/file level 2/script/camera.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class camera : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float movementSpeed, rotationSpeed, maxHeight, minHeight;
+    [SerializeField] private Camera cam;
+    private Rigidbody rb;
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        if (scroll > 0 && transform.position.y < minHeight) scroll = 0;
+        else if (scroll < 0 && transform.position.y > maxHeight) scroll = 0;
+        float moveX = Input.GetAxis("Horizontal") * Time.deltaTime;
+        float moveY = Input.GetAxis("Vertical") * Time.deltaTime;
+        if (moveX != 0f || moveY != 0f || scroll != 0f)
+        {
+            rb.AddForce(transform.forward * moveY * movementSpeed + transform.right * moveX * movementSpeed + cam.transform.forward * scroll * movementSpeed);
+        }
+
+        float camX = Input.GetAxis("Tactical Horizontal") * Time.deltaTime;
+        transform.Rotate(0, camX * rotationSpeed, 0);
     }
 }
