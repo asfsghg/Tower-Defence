e519674309f0b44ecce9e551f8af5673245fd212
asfsghg <artemryzenka@gmail.com>
2025-07-31 18:57:04 +0300

    Game over panel

--- Assets/Core/Main tower.cs
+++ Assets/Core/Main tower.cs
@@ -1,16 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class Maintower : MonoBehaviour
 {
     [SerializeField] public int _health;
+    [SerializeField] private GameObject gameOverPanel;
 
     void Update()
     {
         if (_health <= 0)
         {
-            Debug.Log("Game Over");
+            gameOverPanel.SetActive(true);
+            Time.timeScale = 0;
         }
     }
 }
